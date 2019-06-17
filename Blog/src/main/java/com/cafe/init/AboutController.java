67ee216b34dc6c79b366d4c6c608e37694deda66
Administrator <Administrator@306-14>
2019-06-17 10:25:49 +0900

    20190617

--- Blog/src/main/java/com/cafe/init/AboutController.java
+++ Blog/src/main/java/com/cafe/init/AboutController.java
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AboutController {
 	
-	
+	//2019-06-17 수정
 		@RequestMapping(value="/about")
 		public String AboutIndex() {
-			
+			System.out.println("수정함");
 			
 			return "about";
 		}
